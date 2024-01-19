ca05907894193bcc98d63cd2ffd0cd6ad298020e
macdonald34 <macdonald.muhavi@student.moringaschool.com>
2024-01-20 01:20:11 +0300

    completed project

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const BASE_URL = 'https://api.themoviedb.org/3';
 const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 const searchURL = BASE_URL + '/search/movie?'+API_KEY;
-//Inserted my db.json on my javascript code so as to fetch data directly
 
+//Inserted my db.json on my javascript code so as to fetch data directly
 const genres = [
     {
       "id": 28,
